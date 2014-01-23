bcd60286485ab53a637c394957d935598be90c37
Hide <hid4enly@gmail.com>
2014-01-23 12:28:30 +0200

    Initial rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '7378e7afa54220191fa267b77704442dccbf1b09c7108bcf4809d835fcac03af1a1a2ac2789e3db55eafb84587212cd27e414323b41430342c088878433f697b'
